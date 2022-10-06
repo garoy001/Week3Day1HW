fd0b0c07811fa1472816094872c1dbfaff74b9f4
garoy001 <geroyceus@gmail.com>
2022-10-05 22:10:13 -0400

    First commit of homework

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Menu data structure
+var menuLinks = [
+	{ text: 'about', href: '/about' },
+	{ text: 'catalog', href: '/catalog' },
+	{ text: 'orders', href: '/orders' },
+	{ text: 'account', href: '/account' },
+];
+
+const mainEl = document.querySelector('main');
+mainEl.style.backgroundColor = 'var(--main-bg)';
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+mainEl.classList.add('flex-ctr');
+
+const topMenuEl = document.querySelector('nav#top-menu');
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add('flex-around');
